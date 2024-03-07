5b7c468323e3c12e3d899203b5ac9ecf5367bf3d
[Github <dudwnsv123@naver.com>
2024-03-07 17:25:55 +0900

    Day-3

--- src/main/java/com/example/abbs/entity/LocalDateTime.java
+++
@@ -1,5 +0,0 @@
-package com.example.abbs.entity;
-
-public class LocalDateTime {
-
-}
--- src/main/java/com/example/abbs/entity/User.java
+++ src/main/java/com/example/abbs/entity/User.java
@@ -7,24 +7,23 @@ public class User {
 	private String pwd;
 	private String uname;
 	private String email;
-	private LocalDate reDAte;
+	private LocalDate regDate;
 	private int isDeleted;
-	private String prodfile;
+	private String profile;
 	private String github;
 	private String insta;
 	private String location;
 	
 	public User() { }
-	public User(String uid, String pwd, String uname, String email, LocalDate reDAte, int isDeleted, String prodfile,
+	public User(String uid, String pwd, String uname, String email, LocalDate regDate, int isDeleted, String profile,
 			String github, String insta, String location) {
-		super();
 		this.uid = uid;
 		this.pwd = pwd;
 		this.uname = uname;
 		this.email = email;
-		this.reDAte = reDAte;
+		this.regDate = regDate;
 		this.isDeleted = isDeleted;
-		this.prodfile = prodfile;
+		this.profile = profile;
 		this.github = github;
 		this.insta = insta;
 		this.location = location;
@@ -32,90 +31,69 @@ public class User {
 	
 	@Override
 	public String toString() {
-		return "User [uid=" + uid + ", pwd=" + pwd + ", uname=" + uname + ", email=" + email + ", reDAte=" + reDAte
-				+ ", isDeleted=" + isDeleted + ", prodfile=" + prodfile + ", github=" + github + ", insta=" + insta
-		
+		return "User [uid=" + uid + ", pwd=" + pwd + ", uname=" + uname + ", email=" + email + ", regDate=" + regDate
+				+ ", isDeleted=" + isDeleted + ", profile=" + profile + ", github=" + github + ", insta=" + insta
 				+ ", location=" + location + "]";
 	}
 	
 	public String getUid() {
 		return uid;
 	}
-	
 	public void setUid(String uid) {
 		this.uid = uid;
 	}
-	
 	public String getPwd() {
 		return pwd;
 	}
-	
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
-	
 	public String getUname() {
 		return uname;
 	}
-	
 	public void setUname(String uname) {
 		this.uname = uname;
 	}
-	
 	public String getEmail() {
 		return email;
 	}
-	
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	public LocalDate getReDAte() {
-		return reDAte;
+	public LocalDate getRegDate() {
+		return regDate;
 	}
-	
-	public void setReDAte(LocalDate reDAte) {
-		this.reDAte = reDAte;
+	public void setRegDate(LocalDate regDate) {
+		this.regDate = regDate;
 	}
-	
 	public int getIsDeleted() {
 		return isDeleted;
 	}
-	
 	public void setIsDeleted(int isDeleted) {
 		this.isDeleted = isDeleted;
 	}
-	
-	public String getProdfile() {
-		return prodfile;
+	public String getProfile() {
+		return profile;
 	}
-	
-	public void setProdfile(String prodfile) {
-		this.prodfile = prodfile;
+	public void setProfile(String profile) {
+		this.profile = profile;
 	}
-	
 	public String getGithub() {
 		return github;
 	}
-	
 	public void setGithub(String github) {
 		this.github = github;
 	}
-	
 	public String getInsta() {
 		return insta;
 	}
-	
 	public void setInsta(String insta) {
 		this.insta = insta;
 	}
-	
 	public String getLocation() {
 		return location;
 	}
-	
 	public void setLocation(String location) {
 		this.location = location;
 	}
-
 }
