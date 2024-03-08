af59338ed3547f2f76223b1efcb67ae91a75fe2c
[Github <dudwnsv123@naver.com>
2024-03-08 15:51:19 +0900

    Day-2

--- src/main/resources/static/js/aside.js
+++ src/main/resources/static/js/aside.js
@@ -10,18 +10,15 @@ $(document).ready(function() {      // 이벤트 등록
 });
 
 function changeStateMsg() {
-	let stateInputVal = $('#statInput').val();	// 사용자가 수정한 글 읽기
-	$('#stateMsgInput').attr({'class': 'mt-2 d-none'});		// 입력창 감추기
-	$.ajax({		// Asynchronous Javascript and XML, 화면의 일부분만 바꿀 때 주로 사용
-		type: 'GET',
-		url: '/abbs/aside/stateMsg',
-		data: {stateMsg: stateLnputVal},
-		success: function(e) {
-			console.log('state message: ', stateInputVal, result );
-			$('#stateMsg').html(statInputval);
-
-			
-		}
-	
-	});
+  let stateInputVal = $('#stateInput').val();    // 사용자가 수정한 글 읽기
+  $('#stateMsgInput').attr({'class': 'mt-2 d-none'});   // 입력창 감추기
+  $.ajax({      // Asynchronous Javascript and XML, 화면의 일부분만 바꿀 때 주로 사용
+    type: 'GET',
+    url: '/abbs/aside/stateMsg',
+    data: {stateMsg: stateInputVal},
+    success: function(result) {
+      console.log('state message:', stateInputVal, result);
+      $('#stateMsg').html(stateInputVal);
+    }
+  });
 }
